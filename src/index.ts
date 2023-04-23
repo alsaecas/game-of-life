b6bdacd3fa5c26ee456871b6c453920ffcbdcc57
Alejandro saez <alejandrosaezc@gmail.com>
2023-04-23 12:50:47 +0200

    feat(index.ts): add function to print the Board status feat(Game.type.ts): add Board type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+import chalk from "chalk";
+import { Board } from "./types/Game.type";
+
+function printBoard(board: Board) {
+  let boardString = "";
+  for (let row of board) {
+    for (let cell of row) {
+      boardString += cell === 0 ? chalk.bgWhite("   ") : chalk.bgBlue("   ");
+    }
+    boardString += "\n";
+  }
+  console.log(boardString);
+}
+
+printBoard([
+  [1, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 1, 0],
+  [0, 0, 0, 1],
+]);
